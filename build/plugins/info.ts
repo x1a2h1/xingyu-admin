4e6fc0b9bee0bc32bf3a2db9ebbf586efdd61bc9
howie166 <howie166@163.com>
2025-06-19 14:41:39 +0800

    chore: 更新 welcome message 中的文档链接

--- build/plugins/info.ts
+++ build/plugins/info.ts
@@ -3,7 +3,7 @@ import gradientString from 'gradient-string';
 import type { Plugin } from 'vite';
 
 const welcomeMessage = gradientString('#646cff', 'magenta').multiline(
-  `您好! 欢迎使用 react-soybean 开源项目\n我们为您精心准备了精美的保姆级文档\nhttps://react-soybean-docs.pages.dev/index-cn?theme=dark`
+  `您好! 欢迎使用 react-soybean 开源项目\n我们为您精心准备了精美的保姆级文档\nhttps://react-docs.soybeanjs.cn/`
 );
 
 const boxenOptions: BoxenOptions = {
