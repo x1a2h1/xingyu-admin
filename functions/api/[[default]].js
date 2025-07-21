551e7fadfe2a6398550bb80ce0b6c6409a664772
Riecko <tanchjim@aol.com>
2025-07-22 01:27:44 +0800

    feat(api): 新增请求代理功能，支持CORS头设置和响应头处理

--- functions/api/[[default]].js
+++ functions/api/[[default]].js
@@ -6,15 +6,10 @@ export async function onRequest({ env, request }) {
     method: request.method
   });
   proxyRequest.headers.set('Host', env.base_url.replace(/^http?:\/\//, ''));
-
   const response = await fetch(proxyRequest);
-
   response.headers.append('Access-Control-Allow-Origin', '*');
-  // 指定哪些 HTTP 方法（如 GET, POST 等）允许访问资源
   response.headers.append('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  // 指定了哪些 HTTP 头可以在正式请求头中出现
-  response.headers.append('Access-Control-Allow-Headers', 'Authorization');
-  // 预检请求的结果可以被缓存多久
+  response.headers.append('Access-Control-Allow-Headers', '*');
   response.headers.append('Access-Control-Max-Age', '86400');
 
   /** 删除响应头 * */
